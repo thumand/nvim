d15c9af69cacfe5cc7e551c9eb41dbe5b663a1b1
to-the-galaxy <michael.thumand@gmail.com>
2024-01-22 22:08:13 +0100

    Python autoformat

--- init.lua
+++ init.lua
@@ -121,6 +121,19 @@ lsp_zero.on_attach(function(client, bufnr)
 
   -- Disable inline diagnostics
   vim.diagnostic.config({ virtual_text = false })
+
+
+  vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    pattern = { "*.py" },
+    desc = "Auto-format Python files after saving",
+    callback = function()
+      local fileName = vim.api.nvim_buf_get_name(0)
+      vim.cmd(":silent !black --preview -q " .. fileName)
+      vim.cmd(":silent !isort --profile black --float-to-top -q " .. fileName)
+      vim.cmd(":silent !docformatter --in-place --black " .. fileName)
+    end,
+    -- group = autocmd_group,
+  })
 end)
 
 -- Using lsp-servers to format files on save
@@ -133,6 +146,7 @@ lsp_zero.format_on_save({
     ['tsserver'] = { 'javascript', 'typescript' },
     ['rust_analyzer'] = { 'rust' },
     ['lua_ls'] = { 'lua' },
+    ['black'] = { 'python' },
   }
 })
 
